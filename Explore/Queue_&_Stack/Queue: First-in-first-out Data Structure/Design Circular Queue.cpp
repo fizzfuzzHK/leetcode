1f6adfe38008ecdc0dceff4d4a3746d99bc13c6a
fizzfuzzhk <fizzfuzzhk@gmail.com>
2020-12-14 21:34:39 +0900

    a

--- Explore/Queue_&_Stack/Queue: First-in-first-out Data Structure/Design Circular Queue.cpp
+++ Explore/Queue_&_Stack/Queue: First-in-first-out Data Structure/Design Circular Queue.cpp
@@ -3,18 +3,16 @@
 
 
 class MyCircularQueue {
-    int head;
     std::vector<int> data;
+    int head;
     int tail;
     // current size
     int cs;
-
     // max size
     int ms;
 
 public:
     MyCircularQueue(int k) {
-        
         data = std::vector<int>(k,0);
         head = 0;
         tail = k-1;
@@ -26,7 +24,6 @@ public:
         if(isFull()){
             return false;
         }
-        
         else{
             tail = (tail+1)%ms;
             data[tail] = value;
@@ -69,18 +66,7 @@ public:
     }
 };
 
-/**
- * Your MyCircularQueue object will be instantiated and called as such:
- * MyCircularQueue* obj = new MyCircularQueue(k);
- * bool param_1 = obj->enQueue(value);
- * bool param_2 = obj->deQueue();
- * int param_3 = obj->Front();
- * int param_4 = obj->Rear();
- * bool param_5 = obj->isEmpty();
- * bool param_6 = obj->isFull();
- */
-int main(void){
-    // Your code here!
+// int main(void){
     // MyCircularQueue q(5);
     // std::cout << q.Front() << std::endl;
     // q.enQueue(5);
@@ -88,5 +74,4 @@ int main(void){
     // if (!q.isEmpty()) {
     //     std::cout << q.Front() << std::endl;
     // }
-   
-}
+// }
